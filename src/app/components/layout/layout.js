3db80c5a2d33eb5a7721c52e41ce14f241ceca89
joco <jveljan@gmail.com>
2016-11-06 19:22:57 +0100

    optional layout sidebar

--- src/app/components/layout/layout.js
+++ src/app/components/layout/layout.js
@@ -4,6 +4,12 @@ import Navbar from 'app/components/navbar/navbar';
 
 export default {
   template: require('./layout.html'),
+  computed: {
+    mainClass() {
+      const sidebarDefined = this.$slots['sidebar'];
+      return sidebarDefined ? 'col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2' : 'col-md-12';
+    }
+  },
   components: {
     navbar: Navbar
   }
