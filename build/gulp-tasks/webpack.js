1bdf781398192f969ffa2f013c15bcdd6ee4a92c
Jovica Veljanovski <j.veljanovski@kezzler.com>
2017-01-31 13:59:37 +0100

    updating versions, clean up

--- build/gulp-tasks/webpack.js
+++ build/gulp-tasks/webpack.js
@@ -8,35 +8,9 @@ gulp.task('test', (done) => {
   done();
 });
 
-gulp.task('debug', (done) => {
-  // const c1 = require('../webpack/webpack-dev.conf');
-  // const c2 = require('../webpack/webpack-dist.conf');
-  // const c0 = require('../webpack/webpack-common.conf');
-  // console.log(c0);
-  // console.log(c1);
-  // console.log(c2);
-  done();
-});
-
 gulp.task('serve', (done) => {
   require('./serve/server');
   done();
-});
-gulp.task('serve1', (done) => {
-  // Start a webpack-dev-server
-  const port = 4044;
-  const cfg = require('../webpack/webpack-dev.conf');
-  var compiler = webpack(cfg);
-  var server = new WebpackDevServer(compiler, {
-    stats: { colors: true }
-  });
-  server.listen(port, "localhost", function(err) {
-      if(err) throw new gutil.PluginError("webpack-dev-server", err);
-      // Server listening
-      gutil.log("[webpack-dev-server]", `http://localhost:${port}/webpack-dev-server/index.html`);
-      // keep the server alive or continue?
-      done();
-  });
 });
 
 gulp.task('compile', (done) => {
@@ -50,7 +24,6 @@ gulp.task('compile', (done) => {
         }));
         done();
   });
-
 });
 
 gulp.task('build', gulp.series('clean', 'copy-files', 'compile'));
