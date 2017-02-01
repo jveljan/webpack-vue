4b2a7210043d44fb1ba0859aa2ba0033564a926c
Jovica Veljanovski <j.veljanovski@kezzler.com>
2017-02-01 15:39:08 +0100

    addign example typescipt service

--- src/app/routes/dashboard/dashboard.js
+++ src/app/routes/dashboard/dashboard.js
@@ -1,5 +1,17 @@
+import egts from '../../service/example-ts-service';
+
 require('./dashboard.less');
 
 export default {
-  template: require('./dashboard.html')
+  template: require('./dashboard.html'),
+  data() {
+    return {
+      name: 'Mike'
+    }
+  },
+  computed: {
+    sayHello() {
+      return egts.sayHallo(this.name)
+    }
+  }
 }
---
+++ src/app/service/example-ts-service.ts
@@ -0,0 +1,7 @@
+class ExampleTsService {
+  sayHallo(name) {
+    return `Halo ${name}`;
+  }
+}
+
+export default new ExampleTsService();
